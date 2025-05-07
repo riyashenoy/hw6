1fd6bef2fe99a18d8690835978d19631de025976
“Riya <riya.shenoy.us@gmail.com>
2025-05-06 22:54:38 -0700

    part 1

--- hash.h
+++ hash.h
@@ -19,16 +19,61 @@ struct MyStringHash {
     // hash function entry point (i.e. this is h(k))
     HASH_INDEX_T operator()(const std::string& k) const
     {
-        // Add your code here
-
-
+        
+        unsigned long long w[5] = {0};
+        int strLength = k.length();
+        int index = 4; 
+        
+        // grabs from back of str 
+        while (strLength > 0 && index >= 0) {
+            unsigned long long grab = 0;
+            unsigned long long place = 1;
+    
+            // last 6 chars from back
+            for (int i = strLength - 1; i >= 0 && i >= strLength - 6; --i) {
+                char grabChar = k[i];
+                HASH_INDEX_T convDigit = letterDigitToNumber(grabChar);
+                grab += convDigit * place;
+                place *= 36;
+            }
+    
+            w[index] = grab;
+            index--;
+            strLength -= 6;
+        }
+        
+        // weighted sum final hash
+        unsigned long long finalRes = 0;
+        for(int i = 0; i < 5; i++) {
+            finalRes = finalRes + rValues[i] * w[i];
+        }
+        
+        return finalRes;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
-        // Add code here or delete this helper function if you do not want it
 
+        // ascii value mapping
+        if (letter >= 'A' && letter <= 'Z') {
+            letter = letter + 32;
+        }
+        
+        // letters
+        if (letter >= 'a' && letter <= 'z') {
+            int val = letter - 97;
+            return static_cast<HASH_INDEX_T>(val);
+        }
+
+        // digits
+        else if (letter >= '0' && letter <= '9') {
+            int val = 26 + (letter - 48);
+            return static_cast<HASH_INDEX_T>(val);
+        }
+        
+        // anyting else
+        return 0;
     }
 
     // Code to generate the random R values
